dca2dab33df057460068d770420d00e15b46dd67
Yacdany <jack20-21@hotmail.com>
2021-08-15 19:48:58 -0600

    Creating navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import logo from './logo.svg';
+
 import './App.css';
-import ClockJs from './Components/Clock/ClockJs';
 import NavBar from './NavBar/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 
 function App() {
   return (
--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,40 +1,37 @@
 import React from 'react'
-import {Navbar,Nav,NavDropdown,Form,Button,FormControl} from 'react-bootstrap'
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import PokemonsPage from '../Pages/PokemonsPage';
 
 export const NavBar = () => {
     return (
-        <Navbar bg="light" expand="lg">
-  <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-  <Navbar.Toggle aria-controls="navbarScroll" />
-  <Navbar.Collapse id="navbarScroll">
-    <Nav
-      className="mr-auto my-2 my-lg-0"
-      style={{ maxHeight: '100px' }}
-      navbarScroll
-    >
-      <Nav.Link href="#action1">Home</Nav.Link>
-      <Nav.Link href="#action2">Link</Nav.Link>
-      <NavDropdown title="Link" id="navbarScrollingDropdown">
-        <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
-      </NavDropdown>
-      <Nav.Link href="#" disabled>
-        Link
-      </Nav.Link>
-    </Nav>
-    <Form className="d-flex">
-      <FormControl
-        type="search"
-        placeholder="Search"
-        className="mr-2"
-        aria-label="Search"
-      />
-      <Button variant="outline-success">Search</Button>
-    </Form>
-  </Navbar.Collapse>
-</Navbar>
+        <Router>
+            <Navbar bg="light" expand="lg">
+                <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav
+                        className="mr-auto my-2 my-lg-0"
+                        style={{ maxHeight: '100px' }}
+                        navbarScroll
+                    >
+                        <Nav.Link href="#action1"><Link to="/">Pokemons List</Link></Nav.Link>
+                        <Nav.Link href="#action2">Movies Search</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+
+            <Switch>
+                <Route exact path="/">
+                    <PokemonsPage/>
+                </Route>
+            </Switch>
+        </Router>
     )
 }
 
